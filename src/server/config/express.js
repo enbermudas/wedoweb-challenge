dcda1b8ff66b7d8dcf4f2648f6985584d815487f
Enrique Bermúdez <enbermudez96@gmail.com>
2021-01-16 15:20:40 -0300

    feat: connect to database

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import helmet from 'helmet';
 import routes from '../routes';
+import db from './sequelize';
 
 const app = express();
 
 // Middlewares
 app.use(helmet());
 app.use(express.json());
+
+// Database
+db.sequelize.sync();
 
 // Routes
 app.use('/api/v1', routes);
