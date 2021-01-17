6081309cd2e499564572b411d838acffaa9b39b4
Enrique Bermúdez <enbermudez96@gmail.com>
2021-01-17 12:53:10 -0300

    feat: update validators index

--- src/server/validators/index.js
+++ src/server/validators/index.js
@@ -1,7 +1,9 @@
 import signup from './signup';
 import signin from './signin';
+import createWebsite from './create-website';
 
 export default {
   signup,
-  signin
+  signin,
+  createWebsite
 };
