2262b117b9672232753bcdf32c0b9d7d7918e5ec
Enrique Bermúdez <enbermudez96@gmail.com>
2021-01-17 12:52:38 -0300

    feat: add createWebsite method to controller

---
+++ src/server/controllers/website.js
@@ -0,0 +1,29 @@
+import db from '../config/sequelize';
+import validators from '../validators';
+
+const Website = db.websites;
+
+const createWebsite = async (req, res) => {
+  const errors = validators.createWebsite(req.body);
+  if (errors.length !== 0) {
+    return res.status(400).send({
+      message: 'Validation failed.',
+      errors
+    });
+  }
+
+  const website = { url: req.body.url };
+
+  try {
+    const data = await Website.create(website);
+    res.status(201).send({ message: 'You have successfully added a new website.', data });
+  } catch (error) {
+    res.status(500).send({
+      message: error.errors
+    });
+  }
+};
+
+export default {
+  createWebsite
+};
