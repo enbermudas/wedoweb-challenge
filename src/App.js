9deb5bf2e9e4929b6f4aec63fa7acfedcb07d224
Enrique Bermúdez <enbermudez96@gmail.com>
2021-01-18 09:40:03 -0300

    feat: add alert display on app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { message } from 'antd';
 import Navbar from './components/Navbar';
+import SignInModal from './components/SignInModal';
+import { connect } from 'react-redux';
 
-const App = () => {
+const App = (props) => {
+  const { alert } = props;
+
+  useEffect(() => {
+    if (alert.type === 'error') message.error(alert.message, 3);
+    if (alert.type === 'success') message.success(alert.message, 3);
+  }, [alert]);
+
   return (
     <div className="App">
       <Navbar />
+
+      <SignInModal />
     </div>
   );
 };
 
-export default App;
+App.propTypes = {
+  alert: PropTypes.shape({
+    message: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    duration: PropTypes.number.isRequired
+  }).isRequired
+};
+
+const mapState = (state) => ({
+  alert: state.alert
+});
+
+export default connect(mapState)(App);
