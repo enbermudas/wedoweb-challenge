33ebd30dab7ca0124960efda733b7713bb0d1645
Enrique Bermúdez <enbermudez96@gmail.com>
2021-01-17 21:01:10 -0300

    feat: add logging to express

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -1,12 +1,30 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
 import routes from '../routes';
 
 const app = express();
 
+// Logger
+app.use(
+  morgan('dev', {
+    skip: (req, res) => res.statusCode < 400 || process.env === 'test',
+    stream: process.stderr
+  })
+);
+
+app.use(
+  morgan('dev', {
+    skip: (req, res) => res.statusCode >= 400 || process.env === 'test',
+    stream: process.stdout
+  })
+);
+
 // Middlewares
 app.use(helmet());
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/api/v1', routes);
